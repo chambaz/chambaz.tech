5242d68108ee7b767fd22a15ca72516391d6bda8
Adam Chambers <adammchambers@gmail.com>
2021-03-15 20:06:45 -0400

    Add scroll lock

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
+import React, { useEffect } from 'react'
+import { disablePageScroll } from 'scroll-lock'
 import './index.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    disablePageScroll()
+  }, [])
   return <Component {...pageProps} />
 }
 
