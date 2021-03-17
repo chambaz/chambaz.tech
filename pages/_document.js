02946868dfb5f56ba9176e38e151c46a307c79b3
Adam Chambers <adammchambers@gmail.com>
2021-03-17 01:05:09 -0400

    Add app / favicon

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,27 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Shippori+Mincho+B1:wght@400;700&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#333333" />
+          <meta name="msapplication-TileColor" content="#000000" />
+          <meta name="theme-color" content="#000000"></meta>
         </Head>
         <body>
           <Main />
