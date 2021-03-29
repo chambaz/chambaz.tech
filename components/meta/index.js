5f72936375b994fa67c5ef678c8e84740a28e0d0
Adam Chambers <adammchambers@gmail.com>
2021-03-29 08:33:20 -0400

    Add Twitter tags to meta component

--- components/meta/index.js
+++ components/meta/index.js
@@ -5,8 +5,13 @@ const Meta = (props) => {
     <Head>
       <title>{props.title}</title>
       <meta name="description" content={props.description} />
+
       <meta property="og:title" content={props.title} />
       <meta property="og:description" content={props.description} />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@chambaz" />
+      <meta name="twitter:creator" content="@chambaz" />
     </Head>
   )
 }
