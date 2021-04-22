f4192be4b923d216c69f4c77d248ab4ad2a1944a
Adam Chambers <adammchambers@gmail.com>
2021-04-21 23:47:31 -0400

    Add social icons and links to description

--- components/description/index.js
+++ components/description/index.js
@@ -1,17 +1,70 @@
 import React from 'react'
+import { FaTwitterSquare } from 'react-icons/fa'
+import { FaMedium } from 'react-icons/fa'
+import { FaGithubSquare } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+import { AiFillCodepenSquare } from 'react-icons/ai'
+import { ImSoundcloud2 } from 'react-icons/im'
 
 import styles from './description.module.css'
 
 const Description = (props) => {
   return (
-    <p ref={props.descriptionRef} className={styles.description}>
-      Head of Technology at{' '}
-      <a href="https://www.digitalsurgeons.com/">Digital Surgeons</a>.
-      <br />
-      Creative engineer, marketer, musician, maker.
-      <br />I write <a href="">here</a>, do creative coding <a href="">here</a>,
-      and make music <a href="">here</a>.
-    </p>
+    <div ref={props.descriptionRef} className={styles.description}>
+      <p>
+        Head of Technology at{' '}
+        <a href="https://www.digitalsurgeons.com/">Digital Surgeons</a>.
+        <br />
+        Creative engineer, marketer, musician, maker.
+        <br />I write <a href="https://medium.com/@chambaz">here</a>, do
+        creative coding <a href="https://codepen.io/chambaz">here</a>, and make
+        music <a href="https://soundcloud.com/sankaku-music">here</a>.
+      </p>
+      <ul className={styles.descriptionList}>
+        <li className={styles.descriptionListItem}>
+          <a
+            className={styles.descriptionListItemLink}
+            href="https://twitter.com/chambaz">
+            <FaTwitterSquare />
+          </a>
+        </li>
+        <li className={styles.descriptionListItem}>
+          <a
+            className={styles.descriptionListItemLink}
+            href="https://www.linkedin.com/in/adamchambersds/">
+            <FaLinkedin />
+          </a>
+        </li>
+        <li className={styles.descriptionListItem}>
+          <a
+            className={styles.descriptionListItemLink}
+            href="https://medium.com/@chambaz">
+            <FaMedium />
+          </a>
+        </li>
+        <li className={styles.descriptionListItem}>
+          <a
+            className={styles.descriptionListItemLink}
+            href="https://github.com/chambaz/">
+            <FaGithubSquare />
+          </a>
+        </li>
+        <li className={styles.descriptionListItem}>
+          <a
+            className={`${styles.descriptionListItemLink} ${styles.descriptionListItemLinkCodePen}`}
+            href="https://codepen.io/chambaz">
+            <AiFillCodepenSquare />
+          </a>
+        </li>
+        <li className={styles.descriptionListItem}>
+          <a
+            className={`${styles.descriptionListItemLink} ${styles.descriptionListItemLinkSoundCloud}`}
+            href="https://soundcloud.com/sankaku-music">
+            <ImSoundcloud2 />
+          </a>
+        </li>
+      </ul>
+    </div>
   )
 }
 
