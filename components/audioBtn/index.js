6efb7de690f0347f4fc3c6dea70993ecdeaac5c6
Adam Chambers <adammchambers@gmail.com>
2021-04-24 01:32:28 -0400

    Animated audio hint

--- components/audioBtn/index.js
+++ components/audioBtn/index.js
@@ -1,12 +1,34 @@
+import React, { useEffect, useRef } from 'react'
+import gsap from 'gsap'
 import { IoMdVolumeOff } from 'react-icons/io'
 
 import styles from './audioBtn.module.css'
 
 const AudioBtn = (props) => {
+  const labelRef = useRef(null)
+
+  useEffect(() => {
+    if (labelRef) {
+      gsap.to(labelRef.current, {
+        opacity: 1,
+        duration: 1,
+        repeat: 3,
+        yoyo: true,
+        repeatDelay: 10,
+        delay: 10,
+      })
+    }
+  }, [])
+
   return (
-    <button className={styles.audioBtn}>
-      <IoMdVolumeOff />
-    </button>
+    <div className={styles.audioBtn}>
+      <p ref={labelRef} className={styles.audioBtnLabel}>
+        Enable audio experience
+      </p>
+      <button className={styles.audioBtnBtn}>
+        <IoMdVolumeOff />
+      </button>
+    </div>
   )
 }
 
