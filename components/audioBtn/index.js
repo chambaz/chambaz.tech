2a92df0776c9cff4fdb9c53a6669504a98acfaf1
Adam Chambers <adammchambers@gmail.com>
2021-04-24 01:46:52 -0400

    Toggle audioBtn icon on click and cancel hint animation

--- components/audioBtn/index.js
+++ components/audioBtn/index.js
@@ -1,32 +1,49 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import gsap from 'gsap'
-import { IoMdVolumeOff } from 'react-icons/io'
+import { IoIosVolumeOff } from 'react-icons/io'
+import { IoIosVolumeHigh } from 'react-icons/io'
 
 import styles from './audioBtn.module.css'
 
 const AudioBtn = (props) => {
+  const [audioPower, setAudioPower] = useState(false)
+  const [hintAnimation, setHintAnimation] = useState(null)
   const labelRef = useRef(null)
+
+  const toggleAudio = (hintAnimation) => {
+    hintAnimation.kill()
+    setAudioPower(!audioPower)
+  }
 
   useEffect(() => {
     if (labelRef) {
-      gsap.to(labelRef.current, {
-        opacity: 1,
-        duration: 2,
-        repeat: 1,
-        yoyo: true,
-        repeatDelay: 7,
-        delay: 12,
-      })
+      setHintAnimation(
+        gsap.to(labelRef.current, {
+          opacity: 1,
+          duration: 2,
+          repeat: 1,
+          yoyo: true,
+          repeatDelay: 7,
+          delay: 12,
+        })
+      )
     }
   }, [])
 
   return (
-    <div className={styles.audioBtn}>
+    <div className={styles.audioBtn} onClick={() => toggleAudio(hintAnimation)}>
       <p ref={labelRef} className={styles.audioBtnLabel}>
         Enable audio experience
       </p>
-      <button className={styles.audioBtnBtn}>
-        <IoMdVolumeOff />
+      <button
+        className={styles.audioBtnBtn}
+        style={{ display: !audioPower ? 'block' : 'none' }}>
+        <IoIosVolumeOff />
+      </button>
+      <button
+        className={styles.audioBtnBtn}
+        style={{ display: audioPower ? 'block' : 'none' }}>
+        <IoIosVolumeHigh />
       </button>
     </div>
   )
